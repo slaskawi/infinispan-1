973f6670ee5e0762f7da8cb57ab4dff0e84a868e
Sebastian Laskawiec <slaskawi@redhat.com>
2018-05-07 18:06:11 +0200

    summit stuff

--- server/build.sh
+++ server/build.sh
@@ -1,6 +1,7 @@
 rm -rf infinispan-server
-cp -r /home/slaskawi/work/infinispan/infinispan/server/integration/build/target/infinispan-server-9.3.0-SNAPSHOT ./infinispan-server
+cp -r ~/archives/infinispan-server-9.2.1.Final ./infinispan-server
+# cp -r /home/slaskawi/work/infinispan/infinispan/server/integration/build/target/distribution/infinispan-server-9.3.0-SNAPSHOT ./infinispan-server
 cp /home/slaskawi/work/infinispan/infinispan-management-console/target/infinispan-management-console-9.2.1-SNAPSHOT.jar ./infinispan-server/modules/system/layers/base/org/jboss/as/console/ispn/infinispan-management-console-9.2.0.Final.jar
-cp /home/slaskawi/work/infinispan/infinispan-listener-optimizations/target/infinispan-listener-optimizations-1.0-SNAPSHOT.jar ./infinispan-server/standalone/deployments/           
-docker build -t slaskawi/jdg-summit .
-docker push slaskawi/jdg-summit
+cp /home/slaskawi/work/infinispan/infinispan-listener-optimizations/target/infinispan-listener-optimizations-1.0-SNAPSHOT.jar ./infinispan-server/standalone/deployments/
+docker build -t slaskawi/jdg-summit-2 .
+docker push slaskawi/jdg-summit-2
